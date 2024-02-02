45c6aaac695a1e05369ea2bcaca022386d77d38b
Danny Burrow <burrowingdan@gmail.com>
2024-02-01 18:14:21 -0800

    add styling, error handling, and a modal to Pokedex

--- pokedex/app.js
+++ pokedex/app.js
@@ -11,10 +11,17 @@ Functionality
     * evolutions
     * stats
     * alphabet
+* modal 
+* √ error handling
+
 */
 
 const typeSelect = document.getElementById('type-select')
 const results = document.getElementById('results')
+const modal = document.getElementById('modal')
+const modalContent = document.getElementById('modal-content')
+
+modal.addEventListener('click', () => modal.style.display = 'none')
 
 typeSelect.addEventListener('change', e => {
     console.log(e.target.value);
@@ -47,8 +54,29 @@ async function showPokemon(monster) {
 
     // add picture
     const pic = document.createElement("img")
-    pic.setAttribute("src", data.sprites.front_default)
+    if (data.sprites.front_default !== null) {
+        pic.setAttribute("src", data.sprites.front_default)
+    } else {
+        pic.setAttribute('src', 'https://www.giantbomb.com/a/uploads/scale_small/9/95666/1879714-pokeball.png')
+        pic.style.height = '96px'
+    }
     card.appendChild(pic)
+
+    card.addEventListener('click', e => {
+        modal.style.display = 'block'
+        modalContent.innerHTML = ''
+        modalContent.appendChild(h3.cloneNode(true))
+        modalContent.appendChild(pic.cloneNode())
+        data.stats.forEach(stat => {
+            const label = document.createElement('h4')
+            label.innerText = stat.stat.name
+            modalContent.append(label)
+
+            const statNum = document.createElement('span')
+            statNum.innerText = stat.base_stat
+            modalContent.append(statNum)
+        })
+    })
 
     results.appendChild(card)
 }
@@ -63,12 +91,12 @@ const buildSelect = async () => {
     const types = await getStyles()
     console.log(typeSelect);
 
-    // const blankOpt = document.createElement('option')
-    // blankOpt.setAttribute('value', '')
-    // blankOpt.setAttribute('disabled', 'true')
-    // blankOpt.setAttribute('selected', 'true')
-    // blankOpt.innerText = 'Select a type'
-    // typeSelect.appendChild(blankOpt)
+    const blankOpt = document.createElement('option')
+    blankOpt.setAttribute('value', '')
+    blankOpt.setAttribute('disabled', 'true')
+    blankOpt.setAttribute('selected', 'true')
+    blankOpt.innerText = 'Select a type'
+    typeSelect.appendChild(blankOpt)
 
     types.forEach(type => {
         const opt = document.createElement('option')
